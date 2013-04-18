70515c3fe4bca796d4bbe46f9a047b1392501646
Brian Ephraim <brianephraim@gmail.com>
2013-04-18 11:12:20 -0400

    initial commit
    regarding
    https://github.com/bminer/node-blade/issues/103#issuecomment-16520955

---
+++ bladebug.js
@@ -0,0 +1,25 @@
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to bladebug.";
+  };
+
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+  Meteor.startup(function () {
+    $("body").append(Meteor.render(Template.parent({
+      jsVarToExpose:'I am a variable assigned to the parent.blade Template from the javascript file as an argument.'
+    })) );
+  });
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
